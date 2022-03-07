d807cc330e21ef8af66b95f4c84efb93b07bb2d3
Adam Andrews <andrewsadam07@gmail.com>
2022-03-07 15:24:41 +0000

    Added Axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
+const options = {
+	method: 'GET',
+	url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+	params: {
+		q: 'London,uk',
+		lat: '0',
+		lon: '0',
+		callback: 'test',
+		id: '2172797',
+		lang: 'null',
+		units: 'imperial',
+		mode: 'xml',
+	},
+	headers: {
+		'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+	},
+};
+
 function App() {
-  return (
-    <div className="App">
-      Weather App
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+        console.log(process.env.RAPID_API_KEY)
+			});
+	}, []);
+	return <div className="App">Weather App
+  {process.env.REACT_APP_SECRET_NAME}</div>;
 }
 
 export default App;
